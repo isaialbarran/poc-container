8471eb2f03090de0120fab564f135f4618ec2616
Isai <isai@aptitudeglobal.com>
2024-01-02 15:37:38 +0100

    POC working

--- src/App.js
+++ src/App.js
@@ -1,17 +1,41 @@
-import React, { Suspense } from 'react';
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
-
-import ChildAppOne from './components/ChildAppOne';
-import ChildAppTwo from './components/ChildAppTwo';
-
-const routeConfig = ROUTE_CONFIG.routes;
-
-const componentMap = {
-  'appOne': ChildAppOne,
-  'appTwo': ChildAppTwo,
-};
+import React, { Suspense, useState, useEffect } from "react";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 
 const App = () => {
+  const [routeConfig, setRouteConfig] = useState({});
+  const [componentMap, setComponentMap] = useState({});
+
+  useEffect(() => {
+    console.log('Fetching config from:', process.env.CHILD_CONFIG_URL);
+
+    fetch(process.env.CHILD_CONFIG_URL)
+      .then((response) => response.json())
+      .then((childConfig) => {
+        const dynamicComponents = {};
+        Object.keys(childConfig.childApps).forEach((appKey) => {
+          console.log('childConfig',childConfig)
+          console.log('appKey',appKey)
+          dynamicComponents[appKey] = React.lazy(() =>
+            import(`./components/${childConfig.childApps[appKey]}`)
+          );
+        });
+        setComponentMap(dynamicComponents);
+
+        fetch(process.env.ROUTE_CONFIG_URL)
+          .then((response) => response.json())
+          .then((routeData) => {
+            console.log('Loaded route config:', routeData.routes);
+            setRouteConfig(routeData.routes);
+          })
+          .catch((error) =>
+            console.error("Error fetching route config:", error)
+          );
+      })
+      .catch((error) =>
+        console.error("Error fetching child app config:", error)
+      );
+  }, []);
+
   return (
     <Router>
       <div>
@@ -23,7 +47,13 @@ const App = () => {
           <Routes>
             {Object.entries(routeConfig).map(([path, appName]) => {
               const Component = componentMap[appName];
-              return <Route key={path} path={path} element={<Component />} />;
+              return (
+                <Route
+                  key={path}
+                  path={path}
+                  element={Component ? <Component /> : null}
+                />
+              );
             })}
           </Routes>
         </Suspense>
--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,41 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const webpack = require('webpack');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const webpack = require("webpack");
+require("dotenv").config();
 
 module.exports = {
-    entry: './src/index.js',
-    output: {
-        path: path.join(__dirname, '/dist'),
-        filename: 'bundle.js'
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader'
-                }
-            }
-        ]
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: './index.html'
-        }),
-        new webpack.DefinePlugin({
-            'CHILD_APP_CONFIG': JSON.stringify(require('./child-config.json')),
-            'ROUTE_CONFIG': JSON.stringify(require('./route-config.json'))
-          })
+  entry: "./src/index.js",
+  output: {
+    path: path.join(__dirname, "/dist"),
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
     ],
-    devServer: {
-        static: path.join(__dirname, 'dist'),
-        port: 3000,
-        open: true
-    }
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./index.html",
+    }),
+    new webpack.DefinePlugin({
+      "process.env.CHILD_CONFIG_URL": JSON.stringify(
+        process.env.CHILD_CONFIG_URL
+      ),
+      "process.env.ROUTE_CONFIG_URL": JSON.stringify(
+        process.env.ROUTE_CONFIG_URL
+      ),
+    }),
+  ],
+  devServer: {
+    static: path.join(__dirname, "dist"),
+    port: 3000,
+    open: true,
+  },
 };
